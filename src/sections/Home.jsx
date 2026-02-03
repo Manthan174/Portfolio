8e6848ff418881214bb76a47eda7690a0e83ec24
Manthan Suhagiya <your_email@example.com>
2026-02-03 11:47:30 +0530

    Fix resume download for GitHub Pages

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -86,15 +86,14 @@ export default function Home() {
             <a href="#projects" className="px-6 py-3 rounded-full text-white bg-gradient-to-r from-[#1cd8d2] via-[#00bf8f] to-[#302b63]">
               View My Work
             </a>
-          <a
-  href="/Portfolio/Manthan_Resume.pdf"
-  // target="_blank"
-  // rel="noopener noreferrer"
+   <a
+  href={`${import.meta.env.BASE_URL}Manthan_Resume.pdf`}
   download
   className="px-6 py-3 rounded-full bg-white text-black"
 >
   My Resume
 </a>
+
 
           </div>
 
